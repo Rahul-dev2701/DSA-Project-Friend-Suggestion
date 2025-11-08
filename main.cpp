ba54a5355c0cf34b79ed012ca13728a3599851f6
Rahul-dev2701 <rahuldara1052006@gmail.com>
2025-11-08 11:44:59 +0530

    added-register-option

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include<string>
 #include<unordered_map>
+#include<sstream>
 using namespace std;
 
 class User {
@@ -55,12 +56,14 @@ public:
     void loadFriends();             //Read friends.csv → fill friends adjacency list
 
     void viewProfile(int userID);
+    User& getUser(int id);              //get user address from userId
     int getUserIdByUsername(const string &uname);
     
     void displayCurrentFriends(int userId);     //Print friend list from friends[user.id]
     vector<int> suggestedFriends(int userId);       //Use mutuals, similarity score, etc. to suggest
     void addFriend(User& A,User& B);                 //Add new connection to both users’ friend lists
     void interact(User A, User B);          //Increase friendship weight between two IDs
+    int getNextUserId();                    //get next available userId
 
     
     void saveUsers();               //after new registration or profile update
@@ -68,6 +71,7 @@ public:
 
 
     int countMutualFriends(int userA, int userB);       //Compare adjacency lists
+    double computeSimilarity(const User &u1, const User &u2);       //Calculate based on school, locality, age, hobbies, etc.
 };
 
 
@@ -93,14 +97,59 @@ int main(){
         cin.ignore();
         if(choice==1){
             //register new user
+            string userName, firstName, lastName, college, location, password; 
+            int age;
+            vector<string> hobbies;
+            cout<<"Enter username: ";
+            getline(cin,userName);
+            if (sn.getUserIdByUsername(userName) != -1) {
+                cout << " Username already exists. Try another.\n";
+                continue;
+            }
+            cout<<"Enter password: ";
+            getline(cin,password);
+            cout<<"Enter first name: ";
+            getline(cin,firstName);
+            cout<<"Enter last name: ";
+            getline(cin,lastName);
+            cout<<"Enter your college: ";
+            getline(cin,college);
+            cout<<"Enter your city: ";
+            getline(cin,location);
+            cout << "Enter age: ";
+            cin >> age;
+            cin.ignore();
+            cout << "Enter hobbies separated by commas: ";
+            //input hobbies
+
+            string hobbyInput;
+            getline(cin, hobbyInput);
+            stringstream ss(hobbyInput);
+            string hobby;
+            while (getline(ss, hobby, ',')) {
+                hobbies.push_back(hobby);
+            }
+
+            int newId = sn.getNextUserId();
+            User u(newId, userName, firstName, lastName, college, location, age, hobbies);
+            sn.registerUser(u);
+            u.password = password;
+            cout<<"registration succesfull";
+            sn.saveUsers();
+            
 
         }
         else if(choice==2){
             //Login to existing account
 
+            
         }
         else if(choice==3){
             //exit
+            cout << "Exiting FriendBook...\n";
+            sn.saveUsers();
+            sn.saveFriends();
+            break;
         }
 
         else cout<<"invalid option. Try again"<<endl;
@@ -109,3 +158,22 @@ int main(){
 
     return 0;
 }
+
+
+//functions
+int SocialNetwork::getNextUserId(){}
+void SocialNetwork::viewProfile(int userID){};
+User& SocialNetwork::getUser(int id){};
+int SocialNetwork::getUserIdByUsername(const string &uname){};
+void SocialNetwork::loadUsers(){};
+void SocialNetwork::loadFriends(){};
+void SocialNetwork::saveUsers(){};
+void SocialNetwork::saveFriends(){};
+void SocialNetwork::registerUser(User A){};
+int SocialNetwork::loginUser(const string &usernme){};
+void SocialNetwork::displayCurrentFriends(int userId){};
+void SocialNetwork::addFriend(User& A, User& B){};
+void SocialNetwork::interact(User A, User B){};
+int SocialNetwork::countMutualFriends(int userA, int userB){};
+double SocialNetwork::computeSimilarity(const User &A, const User &B){};
+vector<int> SocialNetwork::suggestedFriends(int userId){};
